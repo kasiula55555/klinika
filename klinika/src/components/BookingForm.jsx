5b307c3841373ae89e522bf9fa635a3416597a13
kasia Magryta <107499943+kasiula55555@users.noreply.github.com>
2025-12-11 11:09:42 +0100

    gotowe

--- klinika/src/components/BookingForm.jsx
+++ klinika/src/components/BookingForm.jsx
@@ -1,93 +1,84 @@
 import { useState } from "react";
 import { specjalist } from "./Specialists";
 
-function BookingForm({addVisit}){
-    const[name,setName]= useState("");
-    const[specjalist,setSpecjalist]=useState("");
-    const[date,setDate]=useState("");
-}
+function BookingForm({ addVisit }) {
+  const [name, setName] = useState("");
+  const [specjalistId, setSpecjalistId] = useState("");  
+  const [date, setDate] = useState("");
 
-function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
 
-    if(!name||!specjalist||!date){
-        alert("wypelnij wszytskie pola!");
-        return;
+    if (!name || !specjalistId || !date) {
+      alert("Wypełnij wszystkie pola!");
+      return;
     }
 
-    const selectSpecjalist = specjalist.find(
-        (s)=>s.id===Number(specjalistId)
+    const selectedSpecjalist = specjalist.find(
+      (s) => s.id === Number(specjalistId)
     );
 
     addVisit({
-        id:Date.now(),
-        patient:name,
-        specjalist:specjalist.name,
-        date
+      id: Date.now(),
+      patient: name,
+      specjalist: selectedSpecjalist.name,
+      date,
     });
 
-
     setName("");
-    setSpecjalist("");
+    setSpecjalistId("");
     setDate("");
-    
-    
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+      <h2>Umów się na wizytę!</h2>
+
+      <div className="patient" style={{ marginBottom: "10px" }}>
+        <label>
+          Imię i nazwisko:
+          <br />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="np. Anna Nowak"
+          />
+        </label>
+      </div>
+
+      <div className="specjalist" style={{ marginBottom: "10px" }}>
+        <label>
+          Specjalista:
+          <br />
+          <select
+            value={specjalistId}
+            onChange={(e) => setSpecjalistId(e.target.value)}
+          >
+            <option value="">wybierz</option>
+            {specjalist.map((spec) => (
+              <option key={spec.id} value={spec.id}>
+                {spec.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+      <div className="data" style={{ marginBottom: "10px" }}>
+        <label>
+          Data wizyty:
+          <br />
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+      </div>
+
+      <button type="submit">Zapisz wizytę</button>
+    </form>
+  );
 }
 
-
-
-return(
-
-    <form onSubmit={handleSubmit} style={{marginBottom:"20px"}}>
-        <h2>umow sie na wizyte!</h2>
-
-        <div class="patient" style={{marginBottom:"10px"}}>
-            <label>
-                Imie i nazwisko: 
-                <br/>
-                <input
-                value={name}
-                onChange={(e)=>setName(e.target.value)}
-                placeholder="np:anna nowak"
-                />
-            </label>
-        </div>
-
-        <div class="specjalist" style={{marginBotton:"10px"}}>
-            <label>
-                Specjalista:
-                <br/>
-                <select 
-                value={specjalistId}
-                onChange={(e)=> specjalistId(e.target.value)}
-                >
-                <option value="">wybierz</option>
-                {specjalist.map((spec)=>(
-                <option key={spec.id} value ={spec.id}>
-                    {spec.name}
-                </option>
-               ) )}
-               </select>
-            </label>
-        </div>
-
-        <div class="data" style={{marginBottom:"10px"}}>
-            <label>
-                Data wizyty:
-                <br/>
-                <input
-                type="date"
-                value={date}
-                onChange={(e)=>setDate(e.target.value)}
-                />
-            </label>
-        </div>
-
-
-
-
-
-    </form>
-
-
-)
+export default BookingForm;
